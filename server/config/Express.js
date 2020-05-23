d3271378313b7856aaaf7047e6b841ceccfd9545
Ashish Kumar <ashish@Ashishs-MacBook-Air.local>
2020-05-23 15:17:37 +0530

    chore: Morgan http logger added

--- server/config/Express.js
+++ server/config/Express.js
@@ -2,6 +2,17 @@ const express = require("express");
 const Path = require("path");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const httpLogger = morgan((tokens, req, res) => [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, "content-length"), "-",
+    tokens["response-time"](req, res), "ms"
+].join(" "));
+
 
 const init = async (moduleObj) => {
     const app = express();
@@ -13,6 +24,14 @@ const init = async (moduleObj) => {
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
 
+    app.use(httpLogger);
+    const corsOptions = {
+        origin: "*",
+        optionsSuccessStatus: 200
+    };
+
+    // app.options("*", cors(corsOptions));
+    app.use(cors(corsOptions));
     for (let i = 0; i < moduleObj.Route.length; i++) {
         const route = moduleObj.Route[i];
         app.use("/api", route.router);
