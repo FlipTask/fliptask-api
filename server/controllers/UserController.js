b799c27957097d48946fb446661029ee118964e5
Anshul Kushwaha <anshulkushwaha1@gmail.com>
2020-06-04 19:36:41 +0530

    FindOne in user/me

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -3,7 +3,12 @@ const Response = require("./base/Response");
 
 class UserController extends CrudController {
     me = async (req, res, next) => {
-        Response.success(res, req.user);
+        try {
+            const data = await this.service.get(req.user.id, req.query);
+            Response.success(res, data);
+        } catch (error) {
+            Response.error(res, error);
+        }
     }
 
     login = async (req, res, next) => {
