b67424fbacca0bfffbaa4a0153b1776d4acc3c83
Anshul Kushwaha <anshulkushwaha1@gmail.com>
2020-06-04 19:12:40 +0530

    Install script change, package-lock update

--- install.sh
+++ install.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 
-printf "\n------------ Installing Dependencies ----------------\n\n"
-
-npm install;
-
-printf "\n------------ Rebuilding bcrypt package --------------\n\n"
-
-npm rebuild bcrypt --build-from-source;
-
-printf "\n--------- Run (npm run dev) to start server ---------\n\n"
+npm ci;
